3b41ec53d920b74d39c363e81f529aee2895ee27
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-24 10:16:22 -0600

    styles: removing outline from the navMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -6,7 +6,7 @@ export default function NavMenu() {
 
     return (
         <Popover className="relative">
-            <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
+            <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400 focus:outline-none">
                 <Bars3Icon className='w-8 h-8 text-white ' />
             </PopoverButton>
 
