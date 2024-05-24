d58bf30cfe620b571b3ece185d94ba82f23502fa
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-24 12:42:49 -0600

    feat: service that get all projects from the DB, parsing data with the Schema

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { ProjectFormData } from "../types";
+import { ProjectFormData, dashboardProjectSchema } from "../types";
 import { isAxiosError } from "axios";
 
 
@@ -7,6 +7,20 @@ export async function createProject(formData: ProjectFormData) {
     try {
         const { data } = await api.post('/projects', formData);
         return data;
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects');
+        const response = dashboardProjectSchema.safeParse(data);
+        if (response.success) {
+            return response.data;
+        }
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
