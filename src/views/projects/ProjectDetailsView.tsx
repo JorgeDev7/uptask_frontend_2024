3047600c254df5be60bedb161bdb5dedc8ea70b7
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-28 12:02:22 -0600

    feat: Component to display the edit modal

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getProjectById } from "@/api/ProjectAPI";
 import AddTaskModal from "@/components/tasks/AddTaskModal";
 import TaskList from "@/components/tasks/TaskList";
+import EditTaskData from "@/components/tasks/EditTaskData";
 
 export default function ProjectDetailsView() {
 
@@ -35,6 +36,7 @@ export default function ProjectDetailsView() {
                 tasks={data.tasks}
             />
             <AddTaskModal />
+            <EditTaskData />
         </>
     );
 }
