35613d5aabb6d02be29084d2988ae2b2f8841de0
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-28 12:26:45 -0600

    feat: service that call the API to update a task

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -31,3 +31,15 @@ export async function getTaskById({ taskId, projectId }: Pick<TaskAPI, 'taskId' 
         }
     }
 }
+
+export async function updateTask({ formData, projectId, taskId }: Pick<TaskAPI, 'formData' | 'projectId' | 'taskId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}`;
+        const { data } = await api.put<string>(url, formData);
+        return data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            throw new Error(error.response?.data.error);
+        }
+    }
+}
