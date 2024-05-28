bbb2d1aa6e42472347b06e494e1b9468e864d79e
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-28 10:49:44 -0600

    feat: add TaskList component to display tasks

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -2,6 +2,7 @@ import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { useQuery } from '@tanstack/react-query';
 import { getProjectById } from "@/api/ProjectAPI";
 import AddTaskModal from "@/components/tasks/AddTaskModal";
+import TaskList from "@/components/tasks/TaskList";
 
 export default function ProjectDetailsView() {
 
@@ -30,7 +31,9 @@ export default function ProjectDetailsView() {
                     onClick={() => navigate(location.pathname + '?newTask=true')}
                 >Agregar Tarea</button>
             </nav>
-
+            <TaskList
+                tasks={data.tasks}
+            />
             <AddTaskModal />
         </>
     );
