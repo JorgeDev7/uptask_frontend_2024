0ba2cdcccdba7f9937cd77e7459ab81f0e920f61
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-31 12:05:53 -0600

    feat: route for 'forgot-password' and 'new-password'

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,8 @@ import LoginView from './views/auth/LoginView';
 import RegisterView from './views/auth/RegisterView';
 import ConfirmAccountView from './views/auth/ConfirmAccountView';
 import RequestNewCodeView from './views/auth/RequestNewCodeView';
+import ForgotPasswordView from './views/auth/ForgotPasswordView';
+import NewPasswordView from './views/auth/NewPasswordView';
 
 export default function Router() {
     return (
@@ -25,6 +27,8 @@ export default function Router() {
                     <Route path='/auth/register' element={<RegisterView />}></Route>
                     <Route path='/auth/confirm-account' element={<ConfirmAccountView />}></Route>
                     <Route path='/auth/request-code' element={<RequestNewCodeView />}></Route>
+                    <Route path='/auth/forgot-password' element={<ForgotPasswordView />}></Route>
+                    <Route path='/auth/new-password' element={<NewPasswordView />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
