05c9dead41aebb889e97e593e7097d16d2bcdaa8
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-04 11:40:24 -0600

    feat: token passed by props

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -29,7 +29,9 @@ export default function NewPasswordView() {
                     setToken={setToken}
                     setIsValidToken={setIsValidToken}
                 /> :
-                <NewPasswordForm />}
+                <NewPasswordForm
+                    token={token}
+                />}
         </>
     );
 }
