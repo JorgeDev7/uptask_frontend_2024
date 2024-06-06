8bbdec34ad4bc977e0f0683b2fa77357f9d63ca3
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-06 10:58:12 -0600

    feat: logout function

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,12 +2,18 @@ import { Popover, PopoverPanel, PopoverButton, Transition } from '@headlessui/re
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import { Link } from 'react-router-dom';
 import { User } from '../types';
+import { useQueryClient } from '@tanstack/react-query';
 
 type NavMenuProps = {
     name: User['name'];
 };
 
 export default function NavMenu({ name }: NavMenuProps) {
+    const queryClient = useQueryClient();
+    const logout = () => {
+        localStorage.removeItem('AUTH_TOKEN');
+        queryClient.invalidateQueries({ queryKey: ['user'] }); // se debe de invalidar los queries, por que React Query siempre cachea los datos
+    };
 
     return (
         <Popover className="relative">
@@ -37,7 +43,7 @@ export default function NavMenu({ name }: NavMenuProps) {
                         <button
                             className='block p-2 hover:text-purple-950'
                             type='button'
-                            onClick={() => { }}
+                            onClick={logout}
                         >
                             Cerrar Sesión
                         </button>
