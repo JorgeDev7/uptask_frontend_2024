d8f94fa937138ae83f2d5c008333a8aa98411acd
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-06 10:07:55 -0600

    feat: loading text added

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,13 +1,18 @@
 import './spinnerStyles.css';
 export default function Spinner() {
   return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-    </div>
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <div className="sk-chase">
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+          <div className="sk-chase-dot"></div>
+        </div>
+        <p className='mt-5 font-bold text-indigo-800'>Cargando</p>
+      </div>
+    </>
   );
 }
