7f768d69cc5b9ea5f60c0f8bd830062a7be713a7
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-06 10:49:49 -0600

    feat: Protecting the Main Page if the user does not have a token

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Logo from "../components/Logo";
 import NavMenu from "@/components/NavMenu";
+import { userAuth } from "@/hooks/useAuth";
+import Spinner from "@/components/spinner/Spinner";
 
 export default function AppLayout() {
-    return (
+
+    const { data, isError, isLoading } = userAuth();
+
+    if (isLoading) return <Spinner />;
+    if (isError) {
+        return <Navigate to="/auth/login" />;
+    }
+
+    if (data) return (
         <>
             <header className="bg-gray-800 py-5">
                 <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center px-8">
@@ -16,7 +26,6 @@ export default function AppLayout() {
                             <Logo />
                         </Link>
                     </div>
-
                     <NavMenu />
                 </div>
             </header>
