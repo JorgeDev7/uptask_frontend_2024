5b58d810f7661ff4311b70b83dc82770d208a3d9
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-07 10:17:17 -0600

    styles: background color changed

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -30,7 +30,7 @@ export default function ProjectDetailsView() {
             <nav className="my-5 flex gap-3">
                 <button
                     type="button"
-                    className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold transition-colors"
+                    className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold transition-colors"
                     onClick={() => navigate(location.pathname + '?newTask=true')}
                 >Agregar Tarea</button>
             </nav>
