edaf8a99c2ec50fbe25db8aac3bf090e7004e30c
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-11 10:21:11 -0600

    feat: showing task details when is onClick in the task tittle

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -32,7 +32,11 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
     return (
         <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
             <div className=" min-w-0 flex flex-col gap-y-4">
-                <button type="button" className="text-xl font-bold text-slate-600 text-left">
+                <button
+                    type="button"
+                    className="text-xl font-bold text-slate-600 text-left"
+                    onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
+                >
                     {task.name}
                 </button>
                 <p className="text-slate-500">{task.description}</p>
