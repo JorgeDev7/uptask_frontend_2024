072f1be539b1a94d0718f82ca60a53e22b2e8130
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-11 10:04:03 -0600

    feat: add policy to hide actions if is not a manager

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -6,6 +6,7 @@ import { deleteProject, getProjects } from "@/api/ProjectAPI";
 import { toast } from 'react-toastify';
 import Spinner from '@/components/spinner/Spinner';
 import { userAuth } from '@/hooks/useAuth';
+import { isManager } from '@/utils/policies';
 
 export default function DashboardView() {
 
@@ -50,7 +51,7 @@ export default function DashboardView() {
                             <div className="flex min-w-0 gap-x-4">
                                 <div className="min-w-0 flex-auto space-y-2">
                                     <div className='mb-2'>
-                                        {project.manager === user._id ? (
+                                        {isManager(project.manager, user._id) ? (
                                             <p className='font-bold text-xs uppercase bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5'>Manager</p>
                                         ) : (
                                             <p className='font-bold text-xs uppercase bg-green-50 text-green-500 border-2 border-green-500 rounded-lg inline-block py-1 px-5'>Colaborador(a)</p>
@@ -87,7 +88,7 @@ export default function DashboardView() {
                                                     Ver Proyecto
                                                 </Link>
                                             </MenuItem>
-                                            {project.manager === user._id && (
+                                            {isManager(project.manager, user._id) && (
                                                 <>
                                                     <MenuItem>
                                                         <Link to={`/projects/${project._id}/edit`}
