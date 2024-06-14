567b93927b6c05562509ff2dcf830c7a5f4f2091
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-14 11:56:52 -0600

    refactor: add new property to the global type

--- src/api/NoteAPI.ts
+++ src/api/NoteAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios";
-import { NoteFormData, Project, Task } from "../types";
+import { Note, NoteFormData, Project, Task } from "../types";
 import api from "@/lib/axios";
 
 type NoteAPIType = {
     formData: NoteFormData,
     projectId: Project['_id'],
-    taskId: Task['_id'];
+    taskId: Task['_id'],
+    noteId: Note['_id'];
 };
 
 export async function createNote({ projectId, taskId, formData }: Pick<NoteAPIType, 'projectId' | 'taskId' | 'formData'>) {
