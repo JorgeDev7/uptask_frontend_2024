44e3ced4b7e5e177c6ed415021525c67628c1a05
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-14 11:48:04 -0600

    feat: display delete button conditionally if the user is authenticate

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,11 +1,19 @@
+import { userAuth } from "@/hooks/useAuth";
 import { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import Spinner from "../spinner/Spinner";
+import { useMemo } from "react";
 
 type NoteDetailProps = {
     note: Note;
 };
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+    const { data, isLoading } = userAuth();
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+
+    if (isLoading) return <Spinner />;
+
     return (
         <div className="p-3 flex justify-between items-center">
             <div>
@@ -16,6 +24,12 @@ export default function NoteDetail({ note }: NoteDetailProps) {
                     {formatDate(note.createdAt)}
                 </p>
             </div>
+            {canDelete && (
+                <button
+                    type="button"
+                    className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold transition-colors"
+                >Eliminar</button>
+            )}
         </div>
     );
 }
